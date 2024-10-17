e5d22016266133711b54331d06074094b7aaf147
Dina Butko <butkodina@gmail.com>
2024-10-16 23:35:02 -0400

    Update usePageObjects.spec.ts

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -35,10 +35,10 @@ test('parametrized methods @smoke', async({page}) => {
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10)
 })
 
-test.only('testing with argos ci', async({page}) => {
-    const pm = new PageManager(page)
-    await pm.navigateTo().formLayoutsPage()
-    await argosScreenshot(page, "form layouts page");
-    await pm.navigateTo().datepickerPage()
-    await argosScreenshot(page, "datepicker page");
-})
+// test.only('testing with argos ci', async({page}) => {
+//     const pm = new PageManager(page)
+//     await pm.navigateTo().formLayoutsPage()
+//     await argosScreenshot(page, "form layouts page");
+//     await pm.navigateTo().datepickerPage()
+//     await argosScreenshot(page, "datepicker page");
+// })
